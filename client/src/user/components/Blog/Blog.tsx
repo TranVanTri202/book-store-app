8b4d4eec7725a6cf60ed7333b2db01fa1a249697
tranvantritvt <tranvantri270802@gmail.com>
2025-02-21 06:12:05 +0700

    sss

--- client/src/user/components/Blog/Blog.tsx
+++ client/src/user/components/Blog/Blog.tsx
@@ -4,6 +4,7 @@ import "../../components/Blog/Blog.css";
 import { useEffect } from "react";
 import { fetchDataBlog } from "../../../Redux/Slice/BlogSlice";
 import { Col, Row } from "antd";
+
 import Directional from "../Directional/Directional";
 const Blog = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -15,28 +16,7 @@ const Blog = () => {
   return (
     <>
       <Directional directional="Tin Tức" />
-      <div className="blog-home">
-        <Row gutter={16}>
-          {data.map((blog, index) => {
-            return (
-              <Col
-                key={index}
-                xs={{ span: 24 }}
-                md={{ span: 8 }}
-                className="blogs"
-              >
-                <img src={blog.image} alt="" />
-                <div className="heading-blog">{blog.title}</div>
-                <div className="information-blog">
-                  <div className="date">Ngày 1 tháng 1 năm 2024</div>
-                  <div className="text-blog">{blog.description}</div>
-                  <span className="continue">Tiếp tục</span>
-                </div>
-              </Col>
-            );
-          })}
-        </Row>
-      </div>
+      <div className="blog-home">TESST 123321</div>
     </>
   );
 };
